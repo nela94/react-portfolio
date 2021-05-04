f8b477d24af8950348b01517d54c1fc8f5a6f6a2
Manuela Sanchez <m.sanchez947@Hotmail.com>
2021-05-04 14:50:36 -0400

    added aboutMe component and style

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,26 @@
+import manu from '../img/manu.jpeg'
+import styles from '../styles/AboutMe.module.css'
+
 const AboutMe = () => {
   return (
-    <div>
-      <h1>About Me</h1>
+  <div id="aboutmecontained">
+    <div className={styles.aboutmain}>
+      <h1 style={{paddingBottom: '60px', fontSize:'30px'}}>All About Me</h1>
+      <div className={styles.row}>
+        <div className={styles.column}>
+          <img className={styles.imagestyle} src={manu} width={400} height={600} alt="pic of me" />
+        </div>
+        <div className={styles.column}>
+          <p style={{marginTop: '110px',fontSize:'20px', paddingRight: "80px"}}>I was born and raised in Queens, New York but I also claim my Honduran and Salvadoran roots!
+            Before getting into programming I was in the Real Estate industry, where I started as a receptionist
+            and ended up being an agent, all in the span of four years. I loved interacting with all the different type of people
+            I encountered but the love of technology always called my name. I joined a bootcamp where I learned Ruby on Rails,
+            React, Javascript, Redux, SQL, CSS and HTML. I have since then continued learning and now I made a website using a new
+            framework, Next.js. Which is this one! Scroll below to check out the rest of my work and make sure to connect with me!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
