0fdca15f322ddc3ec53f0611c1e653eadda265fd
Manuela Sanchez <m.sanchez947@Hotmail.com>
2021-05-06 14:37:38 -0400

    changed about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -15,8 +15,8 @@ const AboutMe = () => {
             Before getting into programming I was in the Real Estate industry, where I started as a receptionist
             and ended up being an agent, all in the span of four years. I loved interacting with all the different type of people
             I encountered but the love of technology always called my name. I joined a bootcamp where I learned Ruby on Rails,
-            React, Javascript, Redux, SQL, CSS and HTML. I have since then continued learning and now I made a website using a new
-            framework, Next.js. Which is this one! Scroll below to check out the rest of my work and make sure to connect with me!
+            React, Javascript, Redux, SQL, CSS and HTML. I have since then continued learning new technologies and I build projects with what I learn.
+            Scroll below to check out the rest of my work and make sure to connect with me!
             </p>
           </div>
         </div>
