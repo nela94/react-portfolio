b134d5492ee285f0d34a074e4ccdd1f2142a1303
Manuela Sanchez <m.sanchez947@Hotmail.com>
2022-07-12 16:15:20 -0400

    changed bio and pic

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,14 +8,14 @@ const AboutMe = () => {
       <h1 style={{paddingBottom: '60px', fontSize:'30px'}}>All About Me</h1>
       <div className={styles.row}>
         <div className={styles.column}>
-          <img className={styles.imagestyle} src={manu} width={400} height={600} alt="pic of me" />
+          <img className={styles.imagestyle} src={manu} width={450} height={600} alt="pic of me" />
         </div>
         <div className={styles.column}>
           <p className={styles.paragraph}>I was born and raised in Queens, New York but I also claim my Honduran and Salvadoran roots!
             Before getting into programming I was in the Real Estate industry, where I started as a receptionist
             and ended up being an agent, all in the span of four years. I loved interacting with all the different type of people
             I encountered but the love of technology always called my name. I joined a bootcamp where I learned Ruby on Rails,
-            React, Javascript, Redux, SQL, CSS and HTML. I have since then continued learning new technologies and I build projects with what I learn.
+            React, Javascript, Redux, SQL, CSS and HTML. I have since then continued learning new technologies that I use to build new projects with.
             Scroll below to check out the rest of my work and make sure to connect with me!
             </p>
           </div>
